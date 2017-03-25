4e8bdc39601aefff53f280fd600454be20d43dba
Andrew Pullins <android2772@gmail.com>
2017-03-25 10:58:16 -0400

    Setting up Java project

---
+++ src/main/java/com/MindForgeStudios/kata_vending_machine/App.java
@@ -0,0 +1,13 @@
+package com.MindForgeStudios.kata_vending_machine;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
