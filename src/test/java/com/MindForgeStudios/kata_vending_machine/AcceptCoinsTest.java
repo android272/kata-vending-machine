8622dcea8f53c04ef54e22e7f4f5c25427b96eeb
Andrew Pullins <android2772@gmail.com>
2017-03-31 08:42:00 -0400

    changd tests for accept coins unit test

--- src/test/java/com/MindForgeStudios/kata_vending_machine/AcceptCoinsTest.java
+++ src/test/java/com/MindForgeStudios/kata_vending_machine/AcceptCoinsTest.java
@@ -3,21 +3,17 @@ package com.MindForgeStudios.kata_vending_machine;
 import junit.framework.TestCase;
 
 public class AcceptCoinsTest extends TestCase {
-	public void testNull() {
-        assertEquals(false, AcceptCoins.determineValue(null));
-    }
-	
 	public void testFalseValues() {
-		assertEquals(0, AcceptCoins.determineValue(Coin.PENNY));
-		assertEquals(42, AcceptCoins.determineValue(Coin.NICKEL));
-		assertEquals(31, AcceptCoins.determineValue(Coin.DIME));
-		assertEquals(89, AcceptCoins.determineValue(Coin.QUARTER));
+		assertEquals("error", AcceptCoins.determineValue(32, 14.2));
+		assertEquals("error", AcceptCoins.determineValue(11.5, 7));
+		assertEquals("error", AcceptCoins.determineValue(15.4, 5.1));
+		assertEquals("error", AcceptCoins.determineValue(26.3, 6.3));
 	}
 	
 	public void testAcurateValues() {
-		assertEquals(1, AcceptCoins.determineValue(Coin.PENNY));
-		assertEquals(5, AcceptCoins.determineValue(Coin.NICKEL));
-		assertEquals(10, AcceptCoins.determineValue(Coin.DIME));
-		assertEquals(25, AcceptCoins.determineValue(Coin.QUARTER));		
+		assertEquals("return", AcceptCoins.determineValue(19, 2.5));
+		assertEquals(5, AcceptCoins.determineValue(21.2, 5));
+		assertEquals(10, AcceptCoins.determineValue(17.9, 2.2));
+		assertEquals(25, AcceptCoins.determineValue(24.3, 5.6));		
 	}
 }
