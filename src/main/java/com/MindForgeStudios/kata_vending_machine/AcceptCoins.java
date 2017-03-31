2595ef2dc3cd2243ad48089fd674ff72c00f44ce
Andrew Pullins <android2772@gmail.com>
2017-03-31 08:43:17 -0400

    Added code to make unit test pass

--- src/main/java/com/MindForgeStudios/kata_vending_machine/AcceptCoins.java
+++ src/main/java/com/MindForgeStudios/kata_vending_machine/AcceptCoins.java
@@ -1,8 +1,17 @@
 package com.MindForgeStudios.kata_vending_machine;
 
 public class AcceptCoins {
-
-	public static Object determineValue(Object object) {
-		return "value";
+	public static Object determineValue(double diameter, double weight) {
+		if(diameter == 19 && weight == 2.5) {
+			return "return";
+		} else if(diameter == 21.2 && weight == 5) {
+			return 5;
+		} else if(diameter == 17.9 && weight == 2.2) {
+			return 10;
+		} else if(diameter == 24.3 && weight == 5.6) {
+			return 25;
+		} else {
+			return "error";
+		}
 	}
 }
